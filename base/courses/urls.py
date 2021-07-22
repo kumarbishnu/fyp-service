ac1ae519dd83a8fd92be0150f247f93449925b4b
kumarbishnu <kumarbishnu202@gmail.com>
2021-07-23 01:28:11 +0545

    Create Course Create, Update, Delete APIs

--- base/courses/urls.py
+++ base/courses/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     path('', views.get_courses, name='courses'),
     path('<pk>', views.get_course, name='course'),
     path('<pk>/content/', views.get_course_content, name='course_content'),
+
+    # path('enroll/', views.enroll, name='enroll'),
+
+    path('create/', views.create_course, name='course_create'),
+    path('upload/', views.upload_course_image, name='course_image_upload'),
+    path('update/', views.update_course, name='course_update'),
+    path('delete/', views.delete_course, name='course_delete'),
+
 ]
--- base/courses/views.py
+++ base/courses/views.py
@@ -1,7 +1,7 @@
-from .models import Course, Enrollment, Lecture
+from .models import Category, Course, Enrollment, Lecture
 from .serializers import CourseSerializer, CourseSerializerWithContent
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 
@@ -31,7 +31,7 @@ def get_course_content(request, pk):
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAdminUser])
 def upload_course_image(request):
     data = request.data
     course = Course.objects.get(pk=data['course'])
@@ -41,10 +41,55 @@ def upload_course_image(request):
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAdminUser])
 def upload_lecture_file(request):
     data = request.data
     lecture = Lecture.objects.get(pk=data['lecture'])
     lecture.file_content = request.FILES.get('file')
     lecture.save()
     return Response('File Uploaded')
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_course(request):
+    data = request.data
+    user = request.user
+    category = Category.objects.get(pk=data['category'])
+    course = Course.objects.create(
+        title=data['title'],
+        description=data['description'],
+        tutor=user,
+        category=category,
+        level=data['level']
+    )
+    serializer = CourseSerializer(course, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def update_course(request):
+    data = request.data
+    course = Course.objects.get(pk=data['id'])
+    if course.tutor != request.user:
+        return Response('You do not own this course!')
+    category = Category.objects.get(pk=data['category'])
+    course.title = data['title']
+    course.description = data['description']
+    course.category = category
+    course.level = data['level']
+    course.save()
+    serializer = CourseSerializer(course, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def delete_course(request):
+    data = request.data
+    course = Course.objects.get(pk=data['id'])
+    if course.tutor != request.user:
+        return Response('You do not own this course!')
+    course.delete()
+    return Response('Course Deleted!')
